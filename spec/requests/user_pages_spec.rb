56674cd251fe81a00180e04ad19a86511f0a3194
enson <gf_23@qq.com>
2014-07-18 14:12:44 +0800

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,4 +18,27 @@ describe "GET /user_pages" do
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
+
+  describe "signup" do
+    before { visit signup_path }
+    let (:submit) { "Create my account" }
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change ( User, :count )
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Gao Feng"
+        fill_in "Email", with: "gf_23@qq.com"
+        fill_in "Password", with: "gaofeng"
+        fill_in "Confirmation", with: "gaofeng"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change ( User, :count ).by(1)
+      end
+    end
+  end
 end
