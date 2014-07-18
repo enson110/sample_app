c5fbfef458ef983a47ec8a706b2b2812f4ebe75d
enson <gf_23@qq.com>
2014-07-18 16:09:38 +0800

    finished chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,7 +24,7 @@ describe "GET /user_pages" do
     let (:submit) { "Create my account" }
     describe "with invalid information" do
       it "should not create a user" do
-        expect { click_button submit }.not_to change ( User, :count )
+        expect { click_button submit }.not_to change(User, :count)
       end
     end
 
@@ -33,11 +33,28 @@ describe "GET /user_pages" do
         fill_in "Name", with: "Gao Feng"
         fill_in "Email", with: "gf_23@qq.com"
         fill_in "Password", with: "gaofeng"
-        fill_in "Confirmation", with: "gaofeng"
+        fill_in "Password confirmation", with: "gaofeng"
       end
 
       it "should create a user" do
-        expect { click_button submit }.to change ( User, :count ).by(1)
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'gf_23@qq.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
+
+    describe "with invalid information" do
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
       end
     end
   end
