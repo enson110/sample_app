2245f5f2dd94488387264b65ebdb033024de018b
enson <gf_23@qq.com>
2014-07-24 17:12:10 +0800

    Finish user edit, update, index, adn destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name "Gao Feng"
-    email "gf_23@qq.com"
+    sequence(:name) { |n| "Person_#{n}" }
+    sequence(:email) { |n| "Person_#{n}@person.com" }
     password "gaofeng"
     password_confirmation "gaofeng"
+
+    factory :admin do
+    	admin true
+    end
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -18,3 +18,15 @@ RSpec::Matchers.define :have_error_message do |message|
     expect(page).to have_selector('div.alert.alert-error', text: message)
   end
 end
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.hash(remember_token))
+  else
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
